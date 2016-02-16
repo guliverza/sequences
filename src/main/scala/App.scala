489f3b7236dd429384e34558fed80710cd727f2b
Andrejs Popovs <apopov@evolutiongaming.com>
2016-02-16 16:53:02 +0200

    Fibbonachi support

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,68 +2,63 @@ import scala.util.Try
 
 
 object App {
-  type Func = (Long, Long) => Long
+  type Func = PartialFunction[List[Long], Long]
   type Comp = (Long) => Long
 
+  val minus: Func = { case a :: b :: z => b - a }
+  val plus: Func = { case a :: b :: z => a + (z.headOption getOrElse b) }
+  val div: Func = { case a :: b :: z if a != 0 && b % a == 0 => b / a }
+  val mult: Func = { case a :: b :: z => a * (z.headOption getOrElse b) }
+  val fiboUp: Func = { case a :: b :: c :: Nil => a * c - b*b }
+  val fiboDown: Func = { case n :: a :: b :: Nil => (n + b*b)/a }
 
-  val minus = new ((Long, Long) => Long) {
-    override def apply(a: Long, b: Long): Long = a - b
-    override def toString: String = "-"
-  }
-  val plus = new ((Long, Long) => Long) {
-    override def apply(a: Long, b: Long): Long = a + b
-    override def toString: String = "+"
-  }
-  val div = new ((Long, Long) => Long) {
-    override def apply(a: Long, b: Long): Long = a / b
-    override def toString: String = "/"
-  }
-  val mult = new ((Long, Long) => Long) {
-    override def apply(a: Long, b: Long): Long = a * b
-    override def toString: String = "*"
-  }
-
-  val Probes: Seq[(Func, Func)] = Seq(minus -> plus, div -> mult)
-
-  def comp(k: Long, f: Func)(n: Long) = f(n, k)
+  val Bricks: Seq[(Func, Func)] = Seq(minus -> plus, div -> mult, fiboUp -> fiboDown)
 
   def main(args: Array[String]) {
-    val sequence = args map (_.toLong)
-    val r = probe(sequence, Nil) orElse probe(sequence.reverse, Nil)
-    r.fold(println("Nothing found")) { r =>
-      val res = r.reverse
+    val sequence = (args map (_.toLong)).toList
+    val pyramid = build(sequence, Nil) orElse build(sequence.reverse, Nil)
+    pyramid.fold(println("Nothing found")) { pyramid =>
+      val res = pyramid.reverse
       println(res)
-      val (num, lastOp) = res.tail.foldLeft(res.head) { case ((num, f), (nextNum, nextF)) =>
-        (f(nextNum, num), nextF)
+      val result = if (pyramid.isEmpty)
+        sequence.last
+      else {
+        val (num, lastOp) = res.tail.foldLeft(res.head._1.last -> res.head._2) {
+          case ((floor, f), (nextNum, nextF)) =>
+            (f(floor +: nextNum.takeRight(2)), nextF)
+        }
+        lastOp(num +: sequence.takeRight(2))
       }
-      val result = lastOp(num, sequence.last)
       println(result)
     }
   }
 
-  def probe(s: Seq[Long], result: Seq[(Long, Func)]): Option[Seq[(Long, Func)]] = {
-    if (s.size < 2) None
-    else if (s.toSet.size == 1) Some(result)
+  def build(seq: Seq[Long], result: Seq[(List[Long], Func)]): Option[Seq[(List[Long], Func)]] = {
+    if (seq.size < 2) None
+    else if (seq.toSet.size == 1) Some(result)
     else {
-      val probes: Seq[Option[Seq[(Long, Func)]]] = Probes collect {
-        new PartialFunction[(Func, Func), Option[Seq[(Long, Func)]]] {
-          var nextResult: Option[Seq[(Long, Func)]] = None
+      val pyramid = Bricks collect {
+        new PartialFunction[(Func, Func), Option[Seq[(List[Long], Func)]]] {
+          var nextResult: Option[Seq[(List[Long], Func)]] = None
 
-          override def isDefinedAt(inOut: (Func, Func)): Boolean = Try {
-            val (split, restore) = inOut
-            val up = (s.tail zip s.init) map { case (a, b) => split(a, b) }
-            nextResult = probe(up, result :+ (up.last -> restore))
+          override def isDefinedAt(floor: (Func, Func)): Boolean = {
+            val (upstairs, downstairs) = floor
+            val up = seq.tails collect {
+              case a :: b :: c => List(a, b) ++ c.headOption
+            } collect upstairs
+            val upList = up.toList
+            if (seq.size - upList.size <= 2) {
+              nextResult = build(upList, result :+ (upList -> downstairs))
+            }
             nextResult.isDefined
-          } getOrElse false
+          }
 
-          override def apply(v1: (Func, Func)): Option[Seq[(Long, Func)]] = nextResult
+          override def apply(v1: (Func, Func)): Option[Seq[(List[Long], Func)]] = nextResult
         }
       }
-      probes.flatten.sortBy(_.size).headOption
+      pyramid.flatten.sortBy(_.size).headOption
 
     }
   }
-
-
 
 }
