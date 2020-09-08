ab6098643fdb43b26e109a17f64c5a95c8e9989d
Andrejs Popovs <andrejs.popovs@prima.dev>
2020-09-08 11:44:33 +0300

    added test case for some archaeological finding

--- build.sbt
+++ build.sbt
@@ -4,13 +4,12 @@ organization := "AMP"
 
 version := "0.1.0-SNAPSHOT"
 
-scalaVersion := "2.11.2"
+scalaVersion := "2.11.12"
 
-crossScalaVersions := Seq("2.10.4", "2.11.2")
+crossScalaVersions := Seq("2.10.4", "2.11.12")
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "2.2.1" % "test",
   "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
 )
 
-initialCommands := "import example._"
--- src/test/scala/AppSpec.scala
+++ src/test/scala/AppSpec.scala
@@ -1,17 +1,33 @@
-import org.scalatest.{Matchers, PropSpec}
-import org.scalatest.prop._
+import org.scalatest._
 
-class CheckSpec
-  extends PropSpec
-  with GeneratorDrivenPropertyChecks
+class AppSpec
+  extends FlatSpec
   with Matchers {
 
-  property ("Addition and multiplication are related") {
-    forAll { (x: Int) =>
-      whenever(x > 0) {
-        x * 2 should be(x + x)
-      }
-    }
+  "sequences" must "detect 4 from 1,2,3" in {
+    App.calcNext(List(1, 2, 3)) should be (Some(4))
+    App.calcNext(List(3, 2, 1)) should be (Some(0))
+  }
+
+  "sequences" must "detect 8 from 2,4,6" in {
+    App.calcNext(List(2, 4, 6)) should be (Some(8))
+    App.calcNext(List(4, 6, 8, 10)) should be (Some(12))
+    App.calcNext(List(12, 10, 8)) should be (Some(6))
+  }
+
+  "sequences" must "detect 16 from 2,4,8" in {
+    App.calcNext(List(1, 2, 4)) should be (Some(8))
+    App.calcNext(List(2, 4, 8)) should be (Some(16))
+    App.calcNext(List(2, 4, 8, 16)) should be (Some(32))
+    App.calcNext(List(128, 64, 32)) should be (Some(16))
+    App.calcNext(List(1, 11, 111, 1111)) should be (Some(11111))
+  }
+
+  "sequences" must "detect 8 from 1,1,2,3,5" in {
+    App.calcNext(List(1, 1, 2, 3, 5)) should be (Some(8))
+    App.calcNext(List(7, 7, 14, 21, 35)) should be (Some(56))
+    App.calcNext(List(2, 3, 5, 8, 13)) should be (Some(21))
+    App.calcNext(List(35, 21, 14, 7, 7)) should be (Some(0))
   }
 
 }
